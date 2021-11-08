1cded740d9dda8e5a418004449a6dcac884886bd
Jaime Andrés Monsalve Ballesteros <70330970+andresmonsalve19@users.noreply.github.com>
2021-11-08 11:46:31 -0500

    listo

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -12,8 +12,14 @@ class Grupo:
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
+        Grupo.asignarNombre("Grado 12")
+        
+    def __str__(self):
+        cadena = "Grupo de estudiantes: grupo predeterminado"
+        return cadena
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self,  **kwargs):
+        self._asignaturas = []
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
@@ -22,16 +28,8 @@ class Grupo:
             lista.append(alumno)
             self.listadoAlumnos = self.listadoAlumnos + lista
         else:
-            self.listadoAlumnos = [alumno]
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+            self.listadoAlumnos = lista + [alumno] 
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
